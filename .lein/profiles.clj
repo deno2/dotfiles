e0a0204463b2f6e189ee2f68ae6df0a020d87b4b
Dene Simpson <denesimpson@googlemail.com>
2014-04-07 12:01:50 +0100

    first commit

---
+++ .lein/profiles.clj
@@ -0,0 +1,13 @@
+{:user {:plugins [[jonase/eastwood "0.1.0"]
+                  [lein-ancient "0.5.4"]
+                  [lein-cljsbuild "0.3.3"]
+                  [lein-cloverage "1.0.2"]
+                  [lein-kibit "0.0.8"]
+                  [lein-release "1.0.5"]
+                  [lein-try "0.4.1"]
+                  [lein-vanity "0.2.0"]
+                  [org.clojars.kurtharriger/lein-depgraph "0.1.0"]
+                  [lein-pprint "1.1.1"]]
+        :dependencies [[clojure-complete "0.2.3"]
+                       [criterium "0.4.3"]
+                       [slamhound "1.5.2"]]}}
